65e53095ba1149d95567c81903ac5f02e4aa20e0
Harol <harol.rodriguez28@gmail.com>
2024-12-29 23:36:30 -0500

    :bug: fix

--- src/components/utilities/Dependency.jsx
+++ src/components/utilities/Dependency.jsx
@@ -1,28 +1,27 @@
 "use client"
-import React, { useEffect } from 'react';
-import { ScrollToTop } from 'react-simple-scroll-up';
+import Script from "next/script";
+import React, { useEffect } from "react";
+import { ScrollToTop } from "react-simple-scroll-up";
 
 const Dependency = () => {
-
   useEffect(() => {
-    {
+    // WOW.js debería estar ya cargado en el script
+    const wow = new window.WOW({
+      live: false,
+    });
+    wow.init();
 
-      // Bootstrap Js
-      require('bootstrap/dist/js/bootstrap.bundle.js')
-
-      // WowJs
-      const WOW = require('wowjs/dist/wow.js');
-      window.wow = new WOW.WOW({
-        live: false
-      })
-
-      window.wow.init();
-
-    }
+    // Cargar Bootstrap JS
+    require("bootstrap/dist/js/bootstrap.bundle.js");
   }, []);
 
   return (
     <>
+      {/* Cargar WOW.js y usar beforeInteractive para que se cargue antes de la interacción */}
+      <Script
+        src="https://cdnjs.cloudflare.com/ajax/libs/wow/1.1.2/wow.min.js"
+        strategy="beforeInteractive" // Cargar el script antes de la interacción
+      />
       <ScrollToTop symbol={<i className="fal fa-long-arrow-up"></i>} />
     </>
   );
